e608106368caab8cf6cb314317f9c31765c8fac4
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-26 19:06:32 -0600

    Readd github stream component

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,28 @@ import PortfolioView from './pages/portfolio/portfolio';
 import BlogView from './pages/blog/blog';
 import AboutView from './pages/about/about';
 import { useState, useEffect } from 'react';
-// import { Octokit } from "https://cdn.skypack.dev/octokit";
+import { Octokit } from "@octokit/core";
 
-// const apiToken = import.meta.env.VITE_GITHUB_TOKEN;
-// const octokit = new Octokit({ auth: apiToken }, { userAgent: 'Altagrave-Git' });
+const apiToken = import.meta.env.VITE_GITHUB_TOKEN;
+const octokit = new Octokit({ auth: apiToken }, { userAgent: 'Altagrave-Git' });
 
 const App = () => {
   const [gitData, setGitData] = useState([]);
   const [auth, setAuth] = useState(false);
 
-  // useEffect(() => {
-  //   octokit.request('GET /users/Altagrave-Git/repos', {
-  //     sort: 'pushed',
-  //   })
-  //     .then(({ data }) => {
-  //       setGitData(data);
-  //     })
-  //     .catch(err => console.log(err));
-  // }, []);
+  useEffect(() => {
+    octokit.request('GET /users/Altagrave-Git/repos', {
+      sort: 'pushed',
+    })
+      .then(({ data }) => {
+        setGitData(data);
+      })
+      .catch(err => console.log(err));
+  }, []);
 
   console.log(gitData);
 
-  // Check authorization level of current user session
-  useEffect(() => {
-    fetch(import.meta.env.VITE_CHRONICLE_URL)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setAuth(data[1].auth);
-      });
-  }, [])
+
 
   return (
     <BrowserRouter>
@@ -44,7 +36,6 @@ const App = () => {
         <Route path="/" element={<HomeView gitData={gitData} />} />
         <Route path="/portfolio" element={<PortfolioView gitData={gitData} />} />
         <Route path="/blog" element={<BlogView gitData={gitData} />} />
-        <Route path="/about" element={<AboutView gitData={gitData} />} />
       </Routes>
     </BrowserRouter>
   );
