1ae0f128df8e9100dc3784b0de1cbbcf356a9e0b
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-05-29 21:48:39 -0600

    Remove button component

--- src/components/button/button.jsx
+++
@@ -1,9 +0,0 @@
-import './button.scss';
-
-const Button = ({ text, color, onClick }) => {
-  return (
-    <button onClick={onClick} className={`btn btn-${color}`}>{text}</button>
-  )
-}
-
-export default Button;
