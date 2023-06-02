7cd95ccc1f0618c7529997519e40a766b7f0c746
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-01 19:04:05 -0600

    Add user, token, auth states, fetch user event

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,19 @@ import BlogView from './pages/blog/blog';
 import LoginView from './pages/login/login';
 import { useState, useEffect } from 'react';
 import { Octokit } from "@octokit/core";
+import AuthAPI from './api/api';
+import UserPanel from './components/userpanel/userpanel';
 
 const apiToken = import.meta.env.VITE_GITHUB_TOKEN;
 const octokit = new Octokit({ auth: apiToken }, { userAgent: 'Altagrave-Git' });
 
 const App = () => {
   const [gitData, setGitData] = useState([]);
-  const [auth, setAuth] = useState(false);
+  const [user, setUser] = useState({});
+  const [admin, setAdmin] = useState(false);
+  const [token, setToken] = useState();
 
+  // NOTE: Octokit + Vite requires isomorphic fetch
   useEffect(() => {
     octokit.request('GET /users/Altagrave-Git/repos', {
       sort: 'pushed',
@@ -25,16 +30,29 @@ const App = () => {
       .catch(err => console.log(err));
   }, []);
 
-  console.log(gitData);
+  // Set user details if logged in
+  // get token from user.access_token
+  useEffect(() => {
+    AuthAPI.check()
+      .then((data) => {
+        if (data.username && !user.username) {
+          setUser(data);
+          setAdmin(data.is_superuser);
+          setToken(data.access_token);
+        }
+      })
+      .catch(error => console.log(error));
+  }, [user]);
 
   return (
     <BrowserRouter>
       <Header />
+      <UserPanel user={user} />
       <Routes>
         <Route path="/" element={<HomeView gitData={gitData} />} />
-        <Route path="/portfolio" element={<PortfolioView gitData={gitData} />} />
-        <Route path="/blog" element={<BlogView gitData={gitData} />} />
-        <Route path="/login" element={<LoginView />} />
+        <Route path="/portfolio" element={<PortfolioView admin={admin} />} />
+        <Route path="/blog" element={<BlogView />} />
+        <Route path="/login" element={<LoginView token={token} setUser={setUser} setAdmin={setAdmin} />} />
       </Routes>
     </BrowserRouter>
   );
