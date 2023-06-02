daaa698c6d1ac4f2e0f15958d2349149f38a7097
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-01 18:59:27 -0600

    Add dynamic login, logout button

--- src/components/loginbutton/loginbutton.jsx
+++ src/components/loginbutton/loginbutton.jsx
@@ -1,13 +1,24 @@
 import AuthAPI from "../../api/api";
 import { ReactComponent as EchoIcon } from '../../icons/echo.svg';
 import './loginbutton.scss';
+import { useNavigate } from "react-router-dom";
 
-const LoginButton = () => {
+const LoginButton = ({token}) => {
+  const navigate = useNavigate();
+
   return (
+    <>
+    { token ?
+    <button className="echo-logout" onClick={() => {AuthAPI.logout(token).then(navigate('/'))}}>
+      <span>Sign out</span>
+    </button>
+    :
     <button className="echo-login" onClick={() => AuthAPI.authorize()}>
       <EchoIcon />
       <span>Sign in with Echo</span>
     </button>
+    }
+    </>
   )
 }
 
