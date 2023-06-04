67d6fabc51ce87cbdbec2d3b7980d093a4fc672a
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-03 21:39:58 -0600

    Add fields to prj form

--- src/forms/projectform.jsx
+++ src/forms/projectform.jsx
@@ -1,60 +1,47 @@
 import './forms.scss';
 import { useState } from 'react';
+import getCookie from '../api/cookie';
+import { ContentAPI } from '../api/api';
 
-function getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-      const cookies = document.cookie.split(';');
-      for (let i = 0; i < cookies.length; i++) {
-          const cookie = cookies[i].trim();
-          // Does this cookie string begin with the name we want?
-          if (cookie.substring(0, name.length + 1) === (name + '=')) {
-              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-              break;
-          }
-      }
-  }
-  return cookieValue;
-}
-const csrftoken = getCookie('csrftoken');
-
-const ProjectForm = () => {
+const ProjectForm = ({token, setFormModal}) => {
   const [name, setName] = useState('');
   const [category, setCategory] = useState('');
+  const [description, setDescription] = useState('');
+  const [website, setWebsite] = useState('');
+  const [repo, setRepo] = useState('');
 
-  const handleSubmit = async () => {
-    const submitUrl = import.meta.env.VITE_CHRONICLE_URL + "/projects/"
-    const data = {
-      name: name,
-      category: category
-    }
-    const response = await fetch(submitUrl, {
-      method: 'POST',
-      credentials: 'include',
-      header: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-      .then(response => {
-        return response.json();
+  const csrftoken = getCookie('csrftoken');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await ContentAPI.project(token,
+      {
+        name: name,
+        category: category,
+        description: description,
+        site: website,
+        repo: repo,
       })
-      .then(data => {
-        return data;
-      })
-      .catch(error => {
-        console.log(error);
-      }
-    );
+      .then(res => console.log(res))
+      .catch(error => console.log(error));
     return response;
   }
 
   return (
-    <form method="POST" encType="application/multidata">
+    <form onSubmit={handleSubmit}>
       <h1 className="title">Add Project</h1>
-      <input type="text" name="name" className="form-line" placeholder="Project Name" value={name} onChange={e => setName(e.target.value)} />
-      <input type="text" name="category" className="form-line" placeholder="Software Category" value={category} onChange={e => setCategory(e.target.value)} />
-      <input type="button" className="form-submit" value="Submit" onClick={() => handleSubmit()} />
+      <input type="text" name="name" className="form-text" placeholder="Project Name" value={name} onChange={e => setName(e.target.value)} autoComplete="false" />
+      <input type="text" name="category" className="form-text" placeholder="Software Category" value={category} onChange={e => setCategory(e.target.value)} />
+      <textarea name="description" className="form-text" placeholder="Project Description"  cols="30" rows="8" required={false} value={description} onChange={e => setDescription(e.target.value)}></textarea>
+      <input type="url" name="site" className="form-text" placeholder="Website" required={false} value={website} onChange={e => setWebsite(e.target.value)} />
+      <input type="url" name="repo" className="form-text" placeholder="Repository" required={false} value={repo} onChange={e => setRepo(e.target.value)} />
+      { name.length && category.length && description.length ?
+        <button type="submit" className="form-submit">Submit</button>
+        :
+        <button type="submit" className="form-submit" disabled>Submit</button>
+      }
+      
     </form>
   )
 }
