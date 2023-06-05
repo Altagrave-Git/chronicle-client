9212a1dae093ed68c6ca44ca93a5227c285faa94
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-04 21:19:55 -0600

    Mod submit for formdata class

--- src/forms/projectform.jsx
+++ src/forms/projectform.jsx
@@ -1,31 +1,29 @@
 import './forms.scss';
 import { useState } from 'react';
-import getCookie from '../api/cookie';
 import { ContentAPI } from '../api/api';
 
 const ProjectForm = ({token, setFormModal}) => {
   const [name, setName] = useState('');
   const [category, setCategory] = useState('');
   const [description, setDescription] = useState('');
-  const [website, setWebsite] = useState('');
+  const [site, setSite] = useState('');
   const [repo, setRepo] = useState('');
-
-  const csrftoken = getCookie('csrftoken');
+  const [image, setImage] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await ContentAPI.project(token,
-      {
-        name: name,
-        category: category,
-        description: description,
-        site: website,
-        repo: repo,
-      })
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("category", category);
+    formData.append("description", description);
+    formData.append("site", site);
+    formData.append("repo", repo);
+    formData.append("image", image);
+
+    ContentAPI.project(token, formData)
       .then(res => console.log(res))
       .catch(error => console.log(error));
-    return response;
   }
 
   return (
@@ -34,8 +32,9 @@ const ProjectForm = ({token, setFormModal}) => {
       <input type="text" name="name" className="form-text" placeholder="Project Name" value={name} onChange={e => setName(e.target.value)} autoComplete="false" />
       <input type="text" name="category" className="form-text" placeholder="Software Category" value={category} onChange={e => setCategory(e.target.value)} />
       <textarea name="description" className="form-text" placeholder="Project Description"  cols="30" rows="8" required={false} value={description} onChange={e => setDescription(e.target.value)}></textarea>
-      <input type="url" name="site" className="form-text" placeholder="Website" required={false} value={website} onChange={e => setWebsite(e.target.value)} />
+      <input type="url" name="site" className="form-text" placeholder="Website" required={false} value={site} onChange={e => setSite(e.target.value)} />
       <input type="url" name="repo" className="form-text" placeholder="Repository" required={false} value={repo} onChange={e => setRepo(e.target.value)} />
+      <input className="form-text" type="file" accept='image/*' name='image' onChange={e => setImage(e.target.files[0])} />
       { name.length && category.length && description.length ?
         <button type="submit" className="form-submit">Submit</button>
         :
