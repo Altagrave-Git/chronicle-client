ee69a81a019c661c0d5c7562223812d9fbe56a4b
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-06 23:16:22 -0600

    Add tech form to modal

--- src/forms/formmodal.jsx
+++ src/forms/formmodal.jsx
@@ -1,8 +1,9 @@
 import './forms.scss';
 import ProjectForm from './projectform';
 import ImageForm from './imageform';
+import TechForm from './techform';
 
-const FormModal = ({ formModal, setFormModal, portfolioData, activeIndex, token }) => {
+const FormModal = ({ formModal, setFormModal, portfolioData, activeIndex, token, project }) => {
 
   return (
   <div className="form-modal-container">
@@ -15,8 +16,11 @@ const FormModal = ({ formModal, setFormModal, portfolioData, activeIndex, token 
         <ProjectForm token={token} setFormModal={setFormModal} />
       }
       {
-        formModal == 'image' &&
+        formModal == "image" &&
         <ImageForm token={token} setFormModal={setFormModal} portfolioData={portfolioData} activeIndex={activeIndex} />
+      }
+      { formModal == "tech" &&
+        <TechForm token={token} portfolioData={portfolioData} activeIndex={activeIndex} />
       }
     </div>
   </div>
