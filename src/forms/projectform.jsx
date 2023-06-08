c3ec1d9fde4cae345dc99fb955dc8f2551f4e261
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-07 19:46:07 -0600

    Add active project editor to form

--- src/forms/projectform.jsx
+++ src/forms/projectform.jsx
@@ -1,14 +1,31 @@
 import './forms.scss';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ContentAPI } from '../api/api';
 
-const ProjectForm = ({token, setFormModal}) => {
+const ProjectForm = ({token, portfolioData, activeIndex}) => {
   const [name, setName] = useState('');
   const [category, setCategory] = useState('');
   const [description, setDescription] = useState('');
   const [site, setSite] = useState('');
   const [repo, setRepo] = useState('');
   const [image, setImage] = useState(null);
+  const [mode, setMode] = useState(false);
+
+  useEffect(() => {
+    if (mode == true) {
+      setName(portfolioData[activeIndex].name);
+      setCategory(portfolioData[activeIndex].category);
+      setDescription(portfolioData[activeIndex].description);
+      setSite(portfolioData[activeIndex].site);
+      setRepo(portfolioData[activeIndex].repo);
+    } else {
+      setName('');
+      setCategory('');
+      setDescription('');
+      setSite('');
+      setRepo('');
+    }
+  }, [mode])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -26,9 +43,45 @@ const ProjectForm = ({token, setFormModal}) => {
       .catch(error => console.log(error));
   }
 
+  const handleEdit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("category", category);
+    formData.append("description", description);
+    formData.append("site", site);
+    formData.append("repo", repo);
+    if (image != null) {
+      formData.append("image", image);
+    }
+    const id = portfolioData[activeIndex].id;
+
+    ContentAPI.editProject(token, formData, id)
+      .then(res => console.log(res))
+      .catch(error => console.log(error));
+  }
+
+  // switch between POST and PUT
+  const switchMode = () => {
+    if (mode == false) {
+      setMode(true);
+    } else {
+      setMode(false)
+    }
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <h1 className="title">Add Project</h1>
+    <form onSubmit={mode ? handleEdit : handleSubmit}>
+      <div className="form-title-container">
+        <input type="button" id="form-left" onClick={() => switchMode()} />
+        { mode ?
+        <h1 className="title-2">Edit Project</h1>
+        :
+        <h1 className="title-2">Add Project</h1>
+        }
+        <input type="button" id="form-right" onClick={() => switchMode()} />
+      </div>
       <input type="text" name="name" className="form-text" placeholder="Project Name" value={name} onChange={e => setName(e.target.value)} autoComplete="false" />
       <input type="text" name="category" className="form-text" placeholder="Software Category" value={category} onChange={e => setCategory(e.target.value)} />
       <textarea name="description" className="form-text" placeholder="Project Description"  cols="30" rows="8" required={false} value={description} onChange={e => setDescription(e.target.value)}></textarea>
