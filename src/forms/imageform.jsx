3b4d0cccf6eda05e3fc62740ca6b7e3f74bb3631
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-10 13:10:23 -0600

    Add section field to image form

--- src/forms/imageform.jsx
+++ src/forms/imageform.jsx
@@ -6,14 +6,20 @@ const ImageForm = ({token, portfolioData, activeIndex}) => {
   const [project, setProject] = useState(portfolioData[activeIndex].id);
   const [image, setImage] = useState(null);
   const [type, setType] =  useState(null);
+  const [section, setSection] = useState("");
+
+  const sections = portfolioData[activeIndex].sections;
 
   const handleSubmit = e => {
     e.preventDefault();
 
     const formData = new FormData();
     formData.append('image', image);
-    formData.append('project', String(project));
     formData.append('type', type);
+    formData.append('project', project);
+    if (section.length > 0) {
+      formData.append("section", section);
+    }
 
     ContentAPI.image(token, project, formData)
       .then(res => console.log(res))
@@ -24,7 +30,7 @@ const ImageForm = ({token, portfolioData, activeIndex}) => {
     <form onSubmit={handleSubmit}>
       <h1 className="title-2">Add Image</h1>
       <input className="form-text" type="file" accept='image/*' name='image' onChange={e => setImage(e.target.files[0])} />
-      <label className="form-text" htmlFor="imagetypes" >Image type:</label>
+      <label className="text" htmlFor="imagetypes" >Image type:</label>
       <select className="form-text" name="type" id="imagetypes" onChange={e => {
         if (e.target.value.length) {
           setType(e.target.value);
@@ -32,12 +38,23 @@ const ImageForm = ({token, portfolioData, activeIndex}) => {
           setType(null);
         }
       }}>
-        <option value=''>--select type--</option>
+        <option value=''>--- select image type ---</option>
         <option value="desktop-display">Desktop</option>
         <option value="mobile-display">Mobile</option>
         <option value="logo">Logo</option>
         <option value="other">Other</option>
       </select>
+      { sections.length > 0 &&
+      <>
+        <label htmlFor="section">For section (optional):</label>
+        <select className="form-text" name="section" id="section" onChange={e => setSection(e.target.value)}>
+          <option value="">---------------</option>
+          { 
+            sections.map((item, index) => <option key={index} value={item.id}>{item.title}</option>)
+          }
+        </select>
+      </>
+      }
       { image && type ?
         <button type="submit" className="form-submit">Submit</button>
         :
