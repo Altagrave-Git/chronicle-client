e6b8f61f9f2622294cfa330bfa56d8d692b82371
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-14 05:04:38 -0600

    Disable admin commands until usable

--- src/components/formbutton/formbutton.jsx
+++ src/components/formbutton/formbutton.jsx
@@ -1,4 +1,4 @@
-const FormButton = ({ setFormModal, formType }) => {
+const FormButton = ({ setFormModal, formType, portfolioData }) => {
 
   const containerClass = `btn-container btn-container-${formType}`
   const buttonClass = `btn-add btn-add-${formType}`
@@ -9,9 +9,15 @@ const FormButton = ({ setFormModal, formType }) => {
 
   return (
   <div className={containerClass}>
+    { portfolioData.length > 0 || formType == "project" ?
     <button className={buttonClass} onClick={() => activate()}>
       + {formType}
     </button>
+    :
+    <button className={buttonClass} onClick={() => activate()} disabled>
+      + {formType}
+    </button>
+    }
   </div>
   )
 }
