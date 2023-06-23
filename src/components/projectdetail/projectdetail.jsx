6afb9dc70be15889d61d3083f95b34c7db91d4f8
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-06-22 20:14:24 -0600

    Add newline detection to content rendering

--- src/components/projectdetail/projectdetail.jsx
+++ src/components/projectdetail/projectdetail.jsx
@@ -3,27 +3,36 @@ import { useEffect } from 'react';
 import { ReactComponent as GithubLogo } from "../../icons/github.svg";
 import { ReactComponent as SiteLogo } from "../../icons/website.svg";
 import TechIcon from '../techicon/techicon';
-import FormButton from '../formbutton/formbutton';
+import { useState } from 'react';
+import ImageReel from '../imagereel/imagereel';
 
 const ProjectDetail = ({ project, handleActive, admin, setFormModal }) => {
+  const [viewImages, setViewImages] = useState(0);
+
   const baseUrl = import.meta.env.VITE_CHRONICLE_URL;
 
   useEffect(() => {
     if (project) {
-      document.querySelector(".project-detail").style.backgroundImage = `url(${baseUrl + project.logo})`;
+      document.querySelector(".project-detail-background").style.backgroundImage = `url(${baseUrl + project.logo})`;
     }
   }, [project])
+
+  console.log(project);
 
   return (
   <>
     { project &&
+    <>
+      <div className="project-detail-background-container">
+        <div className="project-detail-background"></div>
+      </div>
       <div className="project-detail">
         <div className="btn-back" onClick={() => handleActive()}>
           <svg viewBox="6 5 10 15" fill="none"><g strokeWidth="0"></g><g strokeLinecap="round" strokeLinejoin="round"></g><g> <path d="M8 12L16 12" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M11 9L8.08704 11.913V11.913C8.03897 11.961 8.03897 12.039 8.08704 12.087V12.087L11 15" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
           <div>Back</div>
         </div>
         <div className="project-detail__header">
-          <div>
+          <div className="project-detail__header-info">
             <h2 className="project-detail__title">{project.name}</h2>
             <h3 className="project-detail__category">{project.category}</h3>
           </div>
@@ -44,11 +53,12 @@ const ProjectDetail = ({ project, handleActive, admin, setFormModal }) => {
         </div>
         <div className="project-detail__body">
           <div className="project-detail__description">
-            <p>{project.description}</p>
+            { project.description && project.description.split("\n").map((para, index) => {
+              return <p key={index}>{para}</p>
+            })}
           </div>
-          { project.tech &&
-            <div className="project-detail__section">
-              <h4>Technologies</h4>
+          { project.tech && project.tech.length > 0 &&
+            <div className="project-detail__sectio">
               <div className="project-detail__tech">
                   {project.tech.map((tech, index) => {
                     return (
@@ -61,28 +71,16 @@ const ProjectDetail = ({ project, handleActive, admin, setFormModal }) => {
               </div>
             </div>
           }
-          { project.videos &&
-            project.videos.map((item, index) => {
-              return (
-                <div key={index}>
-                  <h6 className="subtitle-2">{item.title}</h6>
-                  <video width="280" height="210" controls>
-                    <source src={baseUrl + item.video} type="video/mp4" />
-                    Your browser does not support the video tag.
-                  </video>
-                </div>
-              )
-            })
-          }
           <div className="project-detail__sections">
             {
               project.sections && project.sections.map((section, index) => {
                 return (
                   <div key={index} className="project-detail__section">
                     <h4>{section.title}</h4>
-                    {section.type === 'text' &&
-
-                      <p>{section.description}</p>
+                    
+                    {section.type === 'text' && section.description.split("\n").map((para, index) => {
+                      return <p key={index}>{para}</p>
+                    })
                     }
                     { section.type === 'list' &&
                       <ul>
@@ -99,10 +97,45 @@ const ProjectDetail = ({ project, handleActive, admin, setFormModal }) => {
                   </div>
                 )
               })
-            }     
+            }
           </div>
+          { (project.videos || project.images) && (project.videos.length > 0 || project.images.length > 0) &&
+            <div className="project-detail__section">
+              <h4 className="project-detail__media-title">Media</h4>
+
+              { project.images && project.images.length > 0 &&
+              <div className="view-images-btn-container">
+                <input className="view-images-btn" type="button" value="View Images" onClick={() => setViewImages(1)} />
+              </div>
+              }
+
+              { viewImages === 1 &&
+                <ImageReel images={project.images.map(image => image.image)} setViewImages={setViewImages} />
+              }
+              { project.videos && project.videos.length > 0 &&
+                <div className="project-detail__videos">
+                  <div className="videos-list">
+                    {
+                      project.videos.map((item, index) => {
+                        return (
+                          <div className="video-card" key={index}>
+                            <video controls>
+                              <source src={baseUrl + item.video} type="video/mp4" />
+                              Your browser does not support the video tag.
+                            </video>
+                            <h3 className="subtitle-2">{item.title}</h3>
+                          </div>
+                        )
+                      })
+                    }
+                  </div>
+                </div>
+              }
+            </div>
+          }
         </div>
       </div>
+      </>
     }
   </>
   )
