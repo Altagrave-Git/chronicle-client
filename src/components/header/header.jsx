b7ee6e19b5382ba4b9147178a252447260c0d586
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-07-01 04:07:47 -0600

    Add inbox, authentication and dropdown to nav bar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { ReactComponent as Logo } from '../../icons/name.svg';
 import './header.scss';
+import AuthAPI from "../../api/api";
+import LoginButton from "../loginbutton/loginbutton";
 
 const headerNav = [
   {
@@ -18,10 +20,9 @@ const headerNav = [
   },
 ];
 
-const Header = () => {
+const Header = ({token, admin}) => {
   const location = useLocation();
   const headerRef = React.useRef(null);
-
   const [active, setActive] = React.useState(0);
 
   React.useEffect(() => {
@@ -35,28 +36,67 @@ const Header = () => {
       document.querySelector("head>title").innerText = `${path.split("/")[1][0].toUpperCase() + path.split("/")[1].slice(1).toLowerCase()} | Damon Turcotte`;
     }
 
+    const menu = document.querySelector(".header-dropdown-menu");
+    if (!menu.classList.contains("hide")) {
+      menu.classList.add("hide");
+    }
+
   }, [location]);
+
+  const handleDropdown = () => {
+    const element = document.querySelector(".header-dropdown-menu");
+    element.classList.toggle("hide");
+  }
+
+  React.useEffect(() => {
+    window.addEventListener("click", (e) => {
+      const menu = document.querySelector(".header-dropdown-menu");
+
+      if (!e.target.closest(".header-menu-btn") && !e.target.closest(".header-dropdown-menu") ) {
+        if (!menu.classList.contains("hide")) {
+          menu.classList.add("hide");
+        }
+      }
+    })
+  }, [])
 
   return (
     <>
       <header className="header" ref={headerRef}>
         <div className="container">
-          <Link className="logo-link" to="/">
+          <Link id="logo-home-link" to={"/"}>
             <Logo />
           </Link>
-          <ul className="header-links">
-            {
-              headerNav.map((nav, index) => (
-                <li key={index} className={index === active ? "active" : ""}>
-                  <Link to={nav.path}>{nav.label}</Link>
-                </li>
-              ))
-            }
-          </ul>
+          <div className="header-menu">
+            <ul className="header-links">
+              {
+                headerNav.map((nav, index) => (
+                  <li key={index} className={index === active ? "active" : ""}>
+                    <Link to={nav.path}>{nav.label}</Link>
+                  </li>
+                ))
+              }
+            </ul>
+            <div className="header-menu-btn" onClick={() => handleDropdown()}>
+              <div className="header-menu-btn-bullet"></div>
+            </div>
+          </div>
         </div>
       </header>
       <div className="header-spacer">
         <div />
+      </div>
+      <div className="header-dropdown">
+        <div className="header-dropdown-container">
+          <div className="header-dropdown-menu hide">
+            { admin &&
+              <Link to={"/inbox"} className="inbox-link">
+                Inbox
+              </Link>
+            }
+            <LoginButton token={token} />
+          </div>
+        </div>
       </div>
     </>
   );
