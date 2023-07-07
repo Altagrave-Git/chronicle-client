078a4636aef6cc6af4c8ad9364c51456f18c52e1
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-07-07 06:17:05 -0600

    Add project cards and card parsing

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import './home.scss';
 import GitStream from "../../components/gitstream/gitstream";
 import Skills from "../../components/skills/skills";
@@ -7,9 +7,12 @@ import { ReactComponent as Linkedin } from '../../icons/linkedin.svg';
 import { ReactComponent as Twitter } from '../../icons/twitter.svg';
 import { ReactComponent as Github } from '../../icons/github.svg';
 import { Link } from "react-router-dom";
-import { useEffect, useCallback } from "react";
+import { useEffect, useState } from "react";
+import resume from "../../images/DamonTurcotteCV.pdf";
+import ProjectCard from "../../components/projectcard/projectcard";
 
-const HomeView = ({token, gitData, setNewMail, admin}) => {
+const HomeView = ({token, gitData, setNewMail, admin, portfolioData}) => {
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
 
@@ -35,6 +38,20 @@ const HomeView = ({token, gitData, setNewMail, admin}) => {
     }
   }, [])
 
+  useEffect(() => {
+    const projectList = [];
+    for (let project of portfolioData) {
+      if (projectList.includes(project)) {
+        break;
+      } else {
+        projectList.push(project);
+      }
+    }
+    setProjects(projectList);
+  }, [portfolioData])
+
+  console.log(projects);
+
   return (
     <>
     <main className='home container'>
@@ -44,14 +61,14 @@ const HomeView = ({token, gitData, setNewMail, admin}) => {
           <div className="bio-top">
             <h1 className="title">Damon Turcotte</h1>
             <p>
-              Self-taught <span className="job">web developer</span> deploying full-stack applications out of Lethbridge, Alberta. I have a knack for - and love of - developing custom software and systems from the ground up.
+              Full stack <span className="job">web developer</span> deploying applications and software out of Lethbridge, Alberta.
             </p>
             <p>
-              Everything here - <Link to="/portfolio" className="link-red">portfolio</Link> included - was built with custom code and is part of an integrated network of applications I'm working to expand.
+              I love building custom software and systems from the ground up, which is why everything on-site - <Link to="/portfolio" className="link-red">portfolio</Link> projects included - was built with custom code and is part of an integrated network of applications I'm working to expand.
             </p>
           </div>
 
-          <div className="bio-middle">
+          {/* <div className="bio-middle">
             <div className="bio-spacer">
 
             </div>
@@ -60,7 +77,7 @@ const HomeView = ({token, gitData, setNewMail, admin}) => {
               <h4>2 - - - Activity</h4>
               <h4>3 - - - Contact</h4>
             </div>
-          </div>
+          </div> */}
     
           <div className="bio-bottom">
             <div className="bio-contact">
@@ -79,7 +96,7 @@ const HomeView = ({token, gitData, setNewMail, admin}) => {
                   <Twitter />
                 </a>
               </div>
-              <input type="button" value="Resume" />
+              <a href={resume} target="_blank"><input type="button" value="Resume" /></a>
             </div>
           </div>
         </div>
@@ -89,6 +106,19 @@ const HomeView = ({token, gitData, setNewMail, admin}) => {
 
       <aside className="home">
         <Skills />
+        { projects && projects.length > 0 &&
+          <div className="home-card-container">
+            <h3 className="subtitle-2">
+              <svg viewBox="0 -3 19 19"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier">  <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-180.000000, -3283.000000)"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M129.204085,3126.419 C129.587463,3126.032 129.587463,3125.405 129.204085,3125.018 L129.191207,3125.005 C128.807829,3124.618 128.186697,3124.618 127.803319,3125.005 L124.287534,3128.553 C123.904155,3128.94 123.904155,3129.568 124.287534,3129.955 L127.803319,3133.503 C128.186697,3133.89 128.807829,3133.89 129.191207,3133.503 L129.204085,3133.49 C129.587463,3133.103 129.587463,3132.476 129.204085,3132.089 L127.090057,3129.955 C126.706679,3129.568 126.706679,3128.94 127.090057,3128.553 L129.204085,3126.419 Z M142.712466,3128.553 L139.196681,3125.005 C138.814294,3124.618 138.192171,3124.618 137.808793,3125.005 L137.795915,3125.018 C137.412537,3125.405 137.412537,3126.032 137.795915,3126.419 L139.910934,3128.553 C140.294312,3128.94 140.294312,3129.568 139.910934,3129.955 L137.795915,3132.089 C137.412537,3132.476 137.412537,3133.103 137.795915,3133.49 L137.808793,3133.503 C138.192171,3133.89 138.814294,3133.89 139.196681,3133.503 L142.712466,3129.955 C143.095845,3129.568 143.095845,3128.94 142.712466,3128.553 L142.712466,3128.553 Z M136.809359,3124.40817 L131.74698,3135.23866 C131.582981,3135.57915 131.295245,3136 130.924037,3136 L130.904396,3136 C130.182602,3136 129.712209,3135.0197 130.031369,3134.3588 L135.064287,3123.63077 C135.228287,3123.29128 135.836165,3123.02511 135.836165,3123.02511 L135.836165,3123 C136.818198,3123 137.127538,3123.74728 136.809359,3124.40817 L136.809359,3124.40817 Z"> </path> </g> </g> </g> </g></svg>  
+              &nbsp;Projects
+            </h3>
+            {projects.map((project, index) => {
+              return (
+                <ProjectCard key={index} project={project} page={"home"} />
+              )
+            })}
+          </div>
+        }
         <GitStream gitData={gitData} />
       </aside>
     </main>
