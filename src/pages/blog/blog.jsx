ef81fbb66cbb3ac1d48ab560079fbf53ed170e45
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-07-22 21:22:43 -0600

    Add initial blog view

---
+++ src/pages/blog/blog.jsx
@@ -0,0 +1,12 @@
+import './blog.scss'
+import { useState, useEffect } from 'react'
+
+const BlogView = ({ admin, token }) => {
+  return (
+    <main className='blog container'>
+      <h1>Blog View</h1>
+    </main>
+  )
+}
+
+export default BlogView;
