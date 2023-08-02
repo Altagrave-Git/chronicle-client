dc89b081f6a02898d6f16c1524e45be5b3c4e0d3
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-08-01 22:07:37 -0600

    Add initial placeholders for form

---
+++ src/forms/blogforms/videoform.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { BlogAPI } from "../../api/api";
+
+const BlogVideoForm = ({ post, token, order, category, slug, setApiCall, item=null }) => {
+  const [text, setText] = useState('');
+  const [size, setSize] = useState('m');
+
+  const handleSubmit = async () => {
+    const form = {
+      type: 'title',
+      order: order,
+      post: post.slug,
+      text: text,
+      size: size,
+    };
+
+    if (item != null) {
+      form['id'] = item.id;
+    }
+
+    BlogAPI.postContent(token, form, category, slug)
+      .then(data => {
+        console.log(data);
+        setApi
+      })
+      .catch(error => console.log(error));
+      setApiCall(true);
+  }
+
+  return (
+    <></>
+  )
+}
+
+export default BlogVideoForm;
