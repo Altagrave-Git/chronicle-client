f3c585d5aeaabf4dcb428edcc9b843827e9d50ac
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-08-04 08:34:21 -0600

    Add api methods

--- src/forms/blogforms/titleform.jsx
+++ src/forms/blogforms/titleform.jsx
@@ -3,11 +3,13 @@ import { BlogAPI } from "../../api/api";
 import { useParams } from "react-router-dom";
 import { ReactComponent as Accept } from "../../icons/check.svg";
 import { ReactComponent as Cancel } from "../../icons/xmark.svg";
+import { ReactComponent as Delete } from "../../icons/trash.svg";
 
 const BlogTitleForm = ({ post, token, order, setApiCall, edit=-1, setEdit, formType='', setFormType, text='', size='m', id=false }) => {
   const { category, slug } = useParams();
   const [textData, setTextData] = useState(text);
   const [sizeData, setSizeData] = useState(size);
+  const [deleteModal, setDeleteModal] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,20 +24,34 @@ const BlogTitleForm = ({ post, token, order, setApiCall, edit=-1, setEdit, formT
 
     if (id) {
       formData.append('id', id);
-    }
-
-    BlogAPI.postContent(token, formData, category, slug)
+      BlogAPI.updateContent(token, formData, category, slug, 'title', id)
       .then(data => {
         console.log(data);
         setApiCall(true);
-        setEdit(false);
+      })
+    } else {
+      BlogAPI.createContent(token, formData, category, slug, 'title')
+        .then(data => {
+          console.log(data);
+          setApiCall(true);
+        })
+        .catch(error => console.log(error));
+    }
+  }
+
+  const handleDelete = async () => {
+    BlogAPI.deleteContent(token, category, slug, 'title', id)
+      .then(data => {
+        setApiCall(true);
       })
       .catch(error => console.log(error));
   }
 
-  console.log(sizeData);
+  console.log(edit, formType);
 
   return (
+    <>
+    <div className=""></div>
     <form className="blogform" onSubmit={handleSubmit}>
       <div className="blog-input-main">
         <input className={`blog-input-text ${sizeData}`} type="text" name="text" id="text" value={textData} onChange={e => setTextData(e.target.value)} placeholder="Title" />
@@ -50,17 +66,31 @@ const BlogTitleForm = ({ post, token, order, setApiCall, edit=-1, setEdit, formT
       </div>
 
       <div className="blog-input-save">
-        <button type='button' className="blog-cancel-btn" onClick={ formType && formType.length > 0 ? (e) => {
+        <button type='button' className="blog-btn-cancel" onClick={ formType && formType.length > 0 ? (e) => {
           e.preventDefault()
-          formType('');
+          setFormType('');
         }:(e) => {
           e.preventDefault();
           setEdit(-1);
           }}><Cancel /></button>
-        <button type="submit" className="blog-save-btn"><Accept /></button>
+        <button type="button" className="blog-btn-delete" onClick={() => setDeleteModal(true)}><Delete /></button>
+        <button type="submit" className="blog-btn-save"><Accept /></button>
+      </div>
+    </form>
+    
+    { deleteModal === true &&
+      <div className="verify-delete-container">
+        <div className="verify-delete">
+          <h3>Are you sure you want to delete this?</h3>
+          <div className="verify-delete-btns">
+            <input type="button" className="verify-delete-yes" value="Yes" onClick={handleDelete} />
+            <input type="button" className="verify-delete-no" value="No" onClick={() => setDeleteModal(false)} />
+          </div>
+        </div>
       </div>
 
-    </form>
+    }
+    </>
   )
 }
 
