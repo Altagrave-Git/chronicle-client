20a4d177705d4d6c629dda1826563df328daab5a
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-08-07 20:34:39 -0600

    Add publish btn, api call, mv apicall state cap

--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -3,11 +3,15 @@ import { useState } from 'react'
 import { useParams, useNavigate, Link } from 'react-router-dom'
 import BlogList from '../../components/bloglist/bloglist'
 import BlogDetail from '../../components/blogdetail/blogdetail'
+import { BlogAPI } from '../../api/api'
 
 const BlogView = ({ admin, token, categories, setCategories }) => {
   const { category, slug } = useParams();
   const navigate = useNavigate();
   const [writePost, setWritePost] = useState(false);
+  const [post, setPost] = useState({});
+  const [apiCall, setApiCall] = useState(true);
+  const [position, setPosition] = useState(0);
 
   const categoryLinks = [
     {
@@ -19,6 +23,21 @@ const BlogView = ({ admin, token, categories, setCategories }) => {
     ...categories
   ]
 
+  const handlePublish = () => {
+    const formData = new FormData();
+    formData.append('title', post.title);
+    formData.append('category', post.category);
+    if (post.published) {
+      formData.append('published', false);
+    } else if (!post.published) {
+      formData.append('published', true);
+    }
+    
+    BlogAPI.updatePost(token, formData, category, slug)
+      .then(() => setApiCall(true))
+      .catch(error => console.log(error));
+  }
+
   return (
     <main className='blog container'>
       <div className='blog-sidebar-container'>
@@ -26,6 +45,15 @@ const BlogView = ({ admin, token, categories, setCategories }) => {
           <div className='blog-sidebar-nav'>
             { admin &&
               <div className='blog-sidebar-makepost-container'>
+                { slug ?
+                  <>
+                  { post.published ?
+                    <input type="button" value="Edit" className='blog-sidebar-unpublish' onClick={handlePublish} />
+                    :
+                    <input type="button" value="Publish" className='blog-sidebar-publish' onClick={handlePublish} />
+                  }
+                  </>
+                :
                 <input type="button" value="Post" className='blog-sidebar-makepost' onClick={
                   () => {
                     if (writePost) {
@@ -35,6 +63,7 @@ const BlogView = ({ admin, token, categories, setCategories }) => {
                     }
                   }}
                 />
+                }
               </div>
             }
 
@@ -66,7 +95,7 @@ const BlogView = ({ admin, token, categories, setCategories }) => {
       </div>
       <section className='blog-main'>
         { slug && slug.length > 0 ?
-          <BlogDetail slug={slug} category={category} categories={categories} admin={admin} token={token} />
+          <BlogDetail slug={slug} category={category} categories={categories} admin={admin} token={token} post={post} setPost={setPost} apiCall={apiCall} setApiCall={setApiCall} />
           :
           <BlogList admin={admin} token={token} categories={categories} setCategories={setCategories} writePost={writePost} />
         }
