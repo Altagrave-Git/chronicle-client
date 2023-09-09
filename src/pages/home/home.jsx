058f80009f7a5bed22ed3ddc141c38adf77c0657
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-09-08 23:29:26 -0600

    Add blog api call to home, render blog post cards

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -11,12 +11,17 @@ import { useEffect, useState } from "react";
 import resume from "../../images/DamonTurcotteCV.pdf";
 import ProjectCard from "../../components/projectcard/projectcard";
 import { useNavigate } from "react-router-dom";
-import selfie from "../../images/selfie.jpg"
+import selfie from "../../images/selfie.jpg";
+import { BlogAPI } from "../../api/api";
 
 const HomeView = ({token, gitData, setNewMail, admin, portfolioData, setActiveIndex}) => {
   const [projects, setProjects] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   const navigate = useNavigate();
+  
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  const serverUrl = import.meta.env.VITE_CHRONICLE_URL;
 
   useEffect(() => {
 
@@ -56,8 +61,15 @@ const HomeView = ({token, gitData, setNewMail, admin, portfolioData, setActiveIn
 
   const click = (key) => {
     setActiveIndex(key);
+    window.scrollTo(0, 0);
     navigate("/portfolio");
   }
+
+  useEffect(() => {
+    BlogAPI.getRecent()
+      .then(data => setPosts(data))
+      .catch(error => console.log(error));
+  }, [])
 
   return (
     <>
@@ -79,17 +91,6 @@ const HomeView = ({token, gitData, setNewMail, admin, portfolioData, setActiveIn
               Check out the rest of the site to see what I do!
             </p>
           </div>
-
-          {/* <div className="bio-middle">
-            <div className="bio-spacer">
-
-            </div>
-            <div className="bio-navigator">
-              <h4>1 - - - Skills</h4>
-              <h4>2 - - - Activity</h4>
-              <h4>3 - - - Contact</h4>
-            </div>
-          </div> */}
     
           <div className="bio-bottom">
             <div className="bio-contact">
@@ -117,7 +118,33 @@ const HomeView = ({token, gitData, setNewMail, admin, portfolioData, setActiveIn
       <section className="placeholder"></section>
 
       <aside className="home">
+        {/* <div className="home-about">
+          <h3 className="subtitle-2">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm80 256h64c44.2 0 80 35.8 80 80c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16c0-44.2 35.8-80 80-80zm-32-96a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zm256-32H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg>
+            &nbsp;About
+          </h3>
+
+          <div className="home-about-card">
+            <h4>Background</h4>
+            <p>
+              So here's a placeholder paragraph that is supposed to contain content that describes a little bit about myself, my background and the services that I offer.
+            </p>
+          </div>
+        </div> */}
+
+        {/* <div className="home-about">
+          <h3 className="subtitle-2">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M184 48H328c4.4 0 8 3.6 8 8V96H176V56c0-4.4 3.6-8 8-8zm-56 8V96H64C28.7 96 0 124.7 0 160v96H192 320 512V160c0-35.3-28.7-64-64-64H384V56c0-30.9-25.1-56-56-56H184c-30.9 0-56 25.1-56 56zM512 288H320v32c0 17.7-14.3 32-32 32H224c-17.7 0-32-14.3-32-32V288H0V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V288z"/></svg>
+            &nbsp;Services
+          </h3>
+        </div> */}
+
+        <h3 className="subtitle-2">
+        <svg xmlns="http://www.w3.org/2000/svg" id="skills-title-icon" viewBox="0 0 512 512"><path d="M152.1 38.2c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 113C-2.3 103.6-2.3 88.4 7 79s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zm0 160c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 273c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zM224 96c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zM160 416c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H192c-17.7 0-32-14.3-32-32zM48 368a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
+          &nbsp;Skills & Tech
+        </h3>
         <Skills />
+
         { projects && projects.length > 0 &&
           <div className="home-card-container">
             <h3 className="subtitle-2">
@@ -131,12 +158,44 @@ const HomeView = ({token, gitData, setNewMail, admin, portfolioData, setActiveIn
             })}
           </div>
         }
+
         <GitStream gitData={gitData} />
+
+        { posts && posts.length > 0 &&
+          <div className="home-posts-container">
+            <h3 className="subtitle-2">
+              <svg className="home-title-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M96 96c0-35.3 28.7-64 64-64H448c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H80c-44.2 0-80-35.8-80-80V128c0-17.7 14.3-32 32-32s32 14.3 32 32V400c0 8.8 7.2 16 16 16s16-7.2 16-16V96zm64 24v80c0 13.3 10.7 24 24 24H296c13.3 0 24-10.7 24-24V120c0-13.3-10.7-24-24-24H184c-13.3 0-24 10.7-24 24zm208-8c0 8.8 7.2 16 16 16h48c8.8 0 16-7.2 16-16s-7.2-16-16-16H384c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16h48c8.8 0 16-7.2 16-16s-7.2-16-16-16H384c-8.8 0-16 7.2-16 16zM160 304c0 8.8 7.2 16 16 16H432c8.8 0 16-7.2 16-16s-7.2-16-16-16H176c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16H432c8.8 0 16-7.2 16-16s-7.2-16-16-16H176c-8.8 0-16 7.2-16 16z"/></svg>
+              &nbsp;Latest Posts
+            </h3>
+            { 
+              posts.map((item, index) => {
+                return (
+                  <div className="home-post" key={index} onClick={() => {
+                    window.scrollTo(0, 0);
+                    navigate(`/blog/${item.category_slug}/${item.slug}`);
+                  }}>
+                    <div className="home-post-image">
+                      <img src={serverUrl + item.image} alt="blog article thumbnail" />
+                    </div>
+
+                    <div className="home-post-info">
+                      <h4 className="home-card-title">{item.title}</h4>
+                      <h5 className="home-card-category">{item.category_name} | {item.pub_date}</h5>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        }
       </aside>
     </main>
-
+    
     <footer className="home-footer">
+      <h2>Contact</h2>
       <MessageForm token={token} setNewMail={setNewMail} admin={admin} />
+      <p style={{color: "#777", textAlign: "center", fontSize: "1rem"}}>© 2023 Damon Turcotte</p>
     </footer>
   </>
   )
